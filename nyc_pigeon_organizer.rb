d44a23ee0e1b9452f98a7a5bbacbc78e71ab24e0
Benjamin Rowe <browe495@gmail.com>
2020-04-27 23:31:14 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,36 @@
+require "pry" 
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  
+  # make array of names 
+  
+  pigeons = data[:color].reduce { | memo, (key, value) |
+    memo = value[0] if !memo
+    memo = memo + value
+    memo
+  }
+  
+  # cleanup array 
+  
+  pigeons = pigeons.drop(1).flatten.uniq
+  
+  # form template hash 
+  
+  answer = pigeons.reduce({}) { | omem, a |
+    
+    
+    omem[a] = { :color => [], :gender => [], :lives => [] }
+    omem
+  }
+  
+# fill hash
+
+  data.each { |(k, v)|
+    v.each { |(color, names_array)|
+      names_array.each { |b|
+        answer[b][k] << color.to_s 
+      }
+    }
+  }
+  
+  answer
 end
