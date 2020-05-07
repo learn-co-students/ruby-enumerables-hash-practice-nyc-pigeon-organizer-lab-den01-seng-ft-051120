ee2b1fa1374c5b0db7c4720aac28d4de8e4b4995
Brook Beyene <brookbeyene37@gmail.com>
2020-05-07 11:44:52 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_data = {}
+  data.each do |top_key, top_value|
+    top_value.each do |inner_key, inner_value|
+      inner_value.each do |name|
+        if new_data[name].nil?
+          new_data[name] = {}
+        end
+        if new_data[name][top_key].nil?
+          new_data[name][top_key] = []
+        end
+        new_data[name][top_key].push(inner_key.to_s)
+      end
+    end 
+  end
+  return new_data
 end
